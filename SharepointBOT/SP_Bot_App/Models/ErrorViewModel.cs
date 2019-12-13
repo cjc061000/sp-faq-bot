5fd5a40e5a4ab42e2da87ab6ac57ceeee7805742
Christian Caicedo <ccaicedo@credera.com>
2019-12-13 13:29:22 -0600

    init commit for solution

---
+++ SharepointBOT/SP_Bot_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SP_Bot_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
