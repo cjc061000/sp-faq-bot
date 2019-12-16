27ee54f1359939d0184eb391ba0b51d713419290
Christian Caicedo <ccaicedo@credera.com>
2019-12-16 16:12:27 -0600

    updated project to hit logic app and display response

--- SharepointBOT/SP_Bot_App/wwwroot/js/luisActions.js
+++ SharepointBOT/SP_Bot_App/wwwroot/js/luisActions.js
@@ -1,8 +1,7 @@
 ﻿/// <reference path="../lib/pnp/pnp.js" />
 /// <reference path="../lib/pnp/fetch.js" />
 
-import { decode } from "querystring";
-import { $pnp } from "../lib/pnp/pnp";
+
 
 //Enter key pressed on query
 //make the call to LUIS using the query text to determine intent
@@ -33,8 +32,41 @@ function QueryTextEnter() {
         //get the intent and the score that LUIS thinks
         //is the top scoring intent
         var intent = data.topScoringIntent.intent;
-        if (data.entities.length > 0) {
-            console.log("entity:", data.entities[0]);
+        var searchTerm = '';
+        if (intent === 'SearchForDocument') {
+            for (var i = 0; i < data.entities.length; i++) {
+                var thisEntity = data.entities[i];
+                if (thisEntity.type === 'SearchTerm') {
+                    searchTerm = thisEntity.entity;
+                    break;
+                }
+            }
+            if (data.entities.length > 0) {
+                console.log("entity:", data.entities[0]);
+            }
+        }
+        if (searchTerm !== '') {
+            // send to logic app
+            var lappCall = jQuery.ajax({
+                url: "https://prod-16.centralus.logic.azure.com:443/workflows/6420a9a03f9b488a9df5a91487b0e3ed/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hPgg-RjwE-LoFOKrwrUR6Ssldtt9tAFEJdUf-9ZyZVM",
+                type: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                data: '{ "searchTerm": "'+searchTerm+'" }'
+            });
+
+            lappCall.done(function (data, textStatus, jqXHR) {
+                console.log("logic app response", data);
+                var spBody = parseSharepointResult(data);
+                console.log('parsed sp body', spBody);
+                var chatResponse = prepareSearchResponse(spBody, searchTerm);
+                PostResponse(chatResponse);
+            });
+            lappCall.fail(function (e, textStatus, errorThrown) {
+                console.log("error calling logic app", e);
+            });
         }
         console.log("intent:", intent);
         console.log("data", data);
@@ -52,7 +84,8 @@ function PostQuestion(question) {
 }
 
 function PostResponse(response) {
-    $("#response").prepend("<div class='response'><div class='message'>" + response + "</div><div class='hillbilly'><img height='50' src='https://www.markrackley.net/wp-content/uploads/2019/02/hillbilly.png'></div></div>");
+    
+    $("#response").prepend("<div class='response'><div class='message'>" + response + "</div><div class='hillbilly'><img height='50' src='/images/noun_Chatbot.svg'></div></div>");
 }
 
 //execute the Logic App that queries the SharePoint List
@@ -79,4 +112,15 @@ function GetFAQ(keyword) {
     //    defer.resolve();
     //});
     //return defer.promise();
+}
+
+function prepareSearchResponse(spBody, searchTerm) {
+    var chatResponse = 'I searched for "' + searchTerm + '" and found ' + spBody.results.length + " responses. Here are the top 5:<br/>";
+    for (var i = 0; i < Math.min(5, spBody.results.length); i++) {
+        var thisLine = 'Title: ' + spBody.results[i].Title + ' <br/>  \
+        Url: <a href='+ spBody.results[i].Path + '>' + spBody.results[i].Path +'</a><br/><br/>';
+        chatResponse += thisLine;
+    }
+    return chatResponse;
+
 }
---
+++ SharepointBOT/SP_Bot_App/wwwroot/js/sharepointObjects.js
@@ -0,0 +1,22 @@
+﻿function parseSharepointResult(sharepointBody) {
+    var retResultList = { results: [] };
+    var table = sharepointBody.d.query.PrimaryQueryResult.RelevantResults.Table;
+    for (var i = 0; i < table.Rows.results.length; i++) {
+        var tableRowCells = table.Rows.results[i].Cells.results;
+        var thisResult = {
+            'Title': '',
+            'Path': '',
+            'Author': '',
+            'LastModifiedTime': ''
+        };
+
+        for (var j = 0; j < tableRowCells.length; j++) {
+            var thisCell = tableRowCells[j];
+            if (thisCell.Key === 'Title' || thisCell.Key === 'Path' || thisCell.Key === 'Author' || thisCell.Key === 'LastModifiedTime') {
+                thisResult[thisCell.Key] = thisCell.Value;
+            }
+        }
+        retResultList.results.push(thisResult);
+    }
+    return retResultList;
+}
